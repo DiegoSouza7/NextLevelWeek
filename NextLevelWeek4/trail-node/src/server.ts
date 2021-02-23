7cc83b1b2bb512831ed7002908491c82a7b6a61f
DiegoSouza7 <diego78523@gmail.com>
2021-02-23 17:24:47 -0300

    2º day 4º NLW node

--- NextLevelWeek4/trail-node/src/server.ts
+++ NextLevelWeek4/trail-node/src/server.ts
@@ -1,9 +1,13 @@
+import 'reflect-metadata'
 import express from 'express'
+import './database'
+import { router } from './routes'
 
 const server = express()
 
-server.get('/users', (req, res) => {
-  return res.json('olá')
-})
+server.use(express.json())
+server.use(router)
+
+
 
 server.listen(3333, () => console.log('Server is running!'))
